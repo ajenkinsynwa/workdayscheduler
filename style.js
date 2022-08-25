3888899c6ec34a69cbfe34fdf47559717e704882
Alex Jenkins <alexjenkins@Alexs-MacBook-Pro.local>
2022-08-25 00:01:52 -0400

    html

---
+++ style.js
@@ -0,0 +1,15 @@
+var currentDay = moment().format('dddd') + " " + moment().format("DO MMM YYYY");
+
+var nineAm = $("#9am");
+var tenAm = $("#10am");
+var eleveneAm = $("#11am");
+var twelveAm = $("#12pm");
+var onePm = $("#13pm");
+var twoPm = $("#14pm");
+var threePm = $("#15pm");
+var fourPm = $("#16pm");
+var fivePm = $("#17pm");
+
+var hour = moment().hours();
+
+
